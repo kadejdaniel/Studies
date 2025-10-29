1bdff87e1afdef301e09ab7b1b4ea35315100a51
daneq <77daniel.kontakt@gmail.com>
2025-10-29 11:08:22 +0100

    quick fix

--- JavaOOP/LAB41/zadania/src/Main.java
+++ JavaOOP/LAB41/zadania/src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-//
-        Okrag[] okregi = Okrag.zad1(4);
-        Okrag.wyswietl(okregi);
 
+//        Okrag[] okregi = Okrag.zad1(4);
+//        Okrag.wyswietl(okregi);
+        int [][] tab  = tabDwuwymiarowa.utworzTablice();
+        tabDwuwymiarowa.obliczStosunek(tab);
     }
 }
--- JavaOOP/LAB41/zadania/src/tabDwuwymiarowa.java
+++ JavaOOP/LAB41/zadania/src/tabDwuwymiarowa.java
@@ -2,25 +2,55 @@ import java.util.Random;
 
 public class tabDwuwymiarowa {
 
-//
 
-    public static void zad1() {
-        int tabDwu[][] = new int[20][20];
-
+    public static int[][] utworzTablice() {
         Random rand = new Random();
-        int size = rand.nextInt(21);
+        int size = rand.nextInt(11) + 10;
         int[][] tablica = new int[size][size];
+
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                tabDwu[i][j] = rand.nextInt(100);
+                if (i == j) {
+                    if (rand.nextBoolean()){
+                        tablica[i][j] = 1;
+                    }else{
+                        tablica[i][j] = -1;
+                    }
+               } else {
+
+                    tablica[i][j] = rand.nextInt(41) - 20;
+                }
             }
         }
+
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                System.out.print(tabDwu[i][j] + " ");
+                System.out.printf("%4d", tablica[i][j]);
             }
             System.out.println();
         }
+
+        return tablica;
+    }
+
+    public static double obliczStosunek(int[][] tablica) {
+        int sumaParzysteWiersze = 0;
+        int sumaNieparzysteKolumny = 0;
+
+        for (int i = 0; i < tablica.length; i++) {
+            for (int j = 0; j < tablica[i].length; j++) {
+                if (i % 2 == 0) { // parzysty wiersz
+                    sumaParzysteWiersze += tablica[i][j];
+                }
+                if (j % 2 == 1) { // nieparzysta kolumna
+                    sumaNieparzysteKolumny += tablica[i][j];
+                }
+            }
+        }
+
+        System.out.println("Suma parzyste wiersze: " + sumaParzysteWiersze);
+        System.out.println("Suma nieparzyste kolumny: " + sumaNieparzysteKolumny);
+
+        return (double) sumaParzysteWiersze / sumaNieparzysteKolumny;
     }
 }
-
